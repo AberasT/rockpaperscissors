df346999caa2679da7eb368dbf237cacce116b5c
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2021-04-08 19:14:13 -0300

    game function base

--- scripts/script.js
+++ scripts/script.js
@@ -8,26 +8,51 @@ function computerPlay() {
         return "Scissors";
     }
 }
-function play(playerSelection,computerSelection) {
+function oneRound(playerSelection,computerSelection) {
+
     if (playerSelection.toLowerCase()==computerSelection.toLowerCase()) {
-        return "Draw!";
+        return ["Draw!",null];
     }else if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection == "Paper") {
-            return "You lose! Paper beats Rock";
+            return ["You lose! Paper beats Rock",false];
         } else {
-            return "You win! Rock beats Scissors"; 
+
+            return ["You win! Rock beats Scissors",true]; 
         }
     }else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "Rock") {
-            return "You win! Paper beats Rock";
+
+            return ["You win! Paper beats Rock",true];
         } else {
-            return "You lose! Scissors beats Paper";
+            return ["You lose! Scissors beats Paper",false];
         }
-    }else {
+    }else if (playerSelection.toLowerCase() == "scissors"){
         if (computerSelection == "Rock") {
-            return "You lose! Rock beats Scissors";
+            return ["You lose! Rock beats Scissors",false];
         } else {
-            return "You win! Scissors beats Paper";
+            return ["You win! Scissors beats Paper",true];
+        }
+    }
+}
+function game() {
+    let result;
+    let wins = 0; 
+    for (let i=1; i<6; i++) {
+        let userChoice = prompt('What do you choose?');
+        result = oneRound(userChoice,computerPlay());
+        console.log(result[0]);
+        switch(result[1]) {
+            case true:
+                console.log('1 point for you')
+                wins++;
+                break;
+            case false:
+                console.log('1 point for the computer')
+                break;
+            case null:
+                console.log('No points')
+                i--;
+                break;
         }
     }
 }
